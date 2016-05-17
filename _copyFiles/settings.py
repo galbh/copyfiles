3b0fc5395249af694a8e176791b46055c2923d81
Gal Ben Haim <gal_ben_haim@yahoo.com>
2016-05-17 16:21:32 +0300

    move all example folders to /examples

--- _copyFiles/settings.py
+++ _copyFiles/settings.py
@@ -5,18 +5,18 @@ import os
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 # Folder to copy files from
-SOURCE_DIRNAME = ROOT_DIR + "\\public"
+SOURCE_DIRNAME = ROOT_DIR + "\\example\\public"
 
 # Folder to copy files to
-DESTINATION_DIRNAME = ROOT_DIR + "\\destination"
+DESTINATION_DIRNAME = ROOT_DIR + "\\example\\destination"
 
 # Destination to drop zipped file
-ZIP_FILE_DESTINATION = ROOT_DIR + "\\zip_file_destination"
+ZIP_FILE_DESTINATION = ROOT_DIR + "\\example\\zip_file_destination"
 
 # Zipped file name no extension needed
 ZIP_FILE_NAME = "gal"
 
 # Folder to zip
-FOLDER_TO_ZIP = ROOT_DIR + "\\folder_to_zip"
+FOLDER_TO_ZIP = ROOT_DIR + "\\example\\folder_to_zip"
 
 
